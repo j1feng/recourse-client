bbb119a091b3ebfa28b6f3e78828e45faa4835d0
jeffrey7377 <fengjiahe1999@gmail.com>
2022-01-22 20:21:18 -0800

    Update ExampleButton.js

--- src/ExampleButton.js
+++ src/ExampleButton.js
@@ -1,8 +1,35 @@
 let ExampleButton = () => {
-    let MakeAlert = () => {return alert("test message.");}
-    return <button onClick={MakeAlert}>
-      Click button to get your loan approved
-      </button>;
+    
+    function httpGetAsync(theUrl, callback){
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() { 
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+              callback(xmlHttp.responseText);
+      }
+      xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+      xmlHttp.send(null);
+      return xmlHttp.responseText;
+    }
+
+    function changeTable(responseText){
+      document.getElementById('button_result_table').innerHTML = responseText;
+    }
+
+    return <div>
+        <button onClick={() => {
+        return httpGetAsync("https://recourse-api.herokuapp.com/recourse", 
+          changeTable);}}>
+        Get My Recourse Action Set!
+        </button>
+        
+        <button onClick={() => {
+          document.getElementById('button_result_table').innerHTML = null;
+        }}>
+        Hide Action Set
+        </button>
+
+        <div id = 'button_result_table'></div>
+      </div>
   }
 
 export default ExampleButton;
