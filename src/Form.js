b86ccc206ca249ef230c794887b1c2a49e01b041
yabutaka <tacky0630@gmail.com>
2022-02-03 14:33:00 -0800

    fixed the alignment issue

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Button, TextField, Select, MenuItem, InputLabel, FormControl, Box, FormHelperText, Container, Stack } from '@mui/material';
+import { Button, TextField, Select, MenuItem, InputLabel, FormControl, 
+    Box, FormHelperText, Container, Divider } from '@mui/material';
 
 let ActionForm = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const [predicted, setPredicted] = useState(0);
+    const [predicted, setPredicted] = useState(-1);
 
     // Handle POST request to the API when form is submitted
     const onSubmit = (user_input) => {
@@ -38,9 +39,10 @@ let ActionForm = () => {
         setPredicted(result.predicted);
         // If credit denied, show a list of actions to revert the decision
         if (!result.predicted) {
-          document.getElementById('button_result_table').innerHTML = result.recourse_actions;
+            const warning_text = "<p>Here are the things you can do to get accepted.</p>";
+            document.getElementById('button_result_table').innerHTML = warning_text + result.recourse_actions;
         } else {
-          document.getElementById('button_result_table').innerHTML = "";
+            document.getElementById('button_result_table').innerHTML = "";
         }
       })
       .catch(function (error) {
@@ -55,117 +57,124 @@ let ActionForm = () => {
     useEffect(() => {}, [])
     
     return (
-        <Container>
-          <h3>Credit Approval Prediction</h3>
-          <b>If you fill in the form below, you will be predicted whether you will get credit or not. In case you are denied, a list of actions that are required to get accepted.</b>
+        <div>
+            <Container>
+                <Box sx={{ my: 3, mx: 2 }}>
+                    <h3>Credit Approval Prediction</h3>
+                    <b>If you fill in the form below, you will be predicted whether you will get credit or not. In case you are denied, a list of actions that are required to get accepted.</b>
+                </Box>
+                <Divider variant="middle" />
+                `<form onSubmit={handleSubmit(onSubmit)}>
+                    <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style) & .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    >
+                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                            <TextField variant="outlined" type="number" label="Age" {...register("Age", {required: true, max: 100, min: 0})} />
+                            <FormHelperText>Required</FormHelperText>
+                        </FormControl>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Box
-              component="form"
-              sx={{
-                '& > :not(style) & .MuiTextField-root': { m: 1, width: '25ch' },
-              }}
-              noValidate
-            >
-              <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <TextField variant="outlined" type="number" label="Age" {...register("Age", {required: true, max: 100, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-              </FormControl>
+                        <FormControl sx={{ m: 1, minWidth: 200 }}>
+                            <InputLabel>Education Level</InputLabel>
+                            <Select label="Age" {...register("EducationLevel", { required: true })}>
+                            <MenuItem value="1">graduate school</MenuItem>
+                            <MenuItem value="2">university</MenuItem>
+                            <MenuItem value="3">high school</MenuItem>
+                            <MenuItem value="0">others</MenuItem>
+                            </Select>
+                            <FormHelperText>Required</FormHelperText>
+                        </FormControl>
 
-              <FormControl sx={{ m: 1, minWidth: 200 }}>
-                <InputLabel>Education Level</InputLabel>
-                <Select label="Age" {...register("EducationLevel", { required: true })}>
-                  <MenuItem value="1">graduate school</MenuItem>
-                  <MenuItem value="2">university</MenuItem>
-                  <MenuItem value="3">high school</MenuItem>
-                  <MenuItem value="0">others</MenuItem>
-                </Select>
-                <FormHelperText>Required</FormHelperText>
-              </FormControl>
+                        <FormControl sx={{ m: 1, minWidth: 200 }}>
+                            <InputLabel>Martial Status</InputLabel>
+                            <Select {...register("Married", { required: true })}>
+                            <MenuItem value="1">Married</MenuItem>
+                            <MenuItem value="0">Single</MenuItem>
+                            </Select>
+                            <FormHelperText>Required</FormHelperText>
+                        </FormControl>
+                    </Box>
 
-              <FormControl sx={{ m: 1, minWidth: 200 }}>
-                <InputLabel>Martial Status</InputLabel>
-                <Select {...register("Married", { required: true })}>
-                  <MenuItem value="1">Married</MenuItem>
-                  <MenuItem value="0">Single</MenuItem>
-                </Select>
-                <FormHelperText>Required</FormHelperText>
-              </FormControl>
-            </Box>
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200}}>
+                        <p>Do you have any history of over due payments?</p>
+                        <Select {...register("HistoryOfOverduePayments", { required: true })}>
+                        <MenuItem value="1">Yes</MenuItem>
+                        <MenuItem value="0">No</MenuItem>
+                        </Select>
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
 
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200}}>
-                <p>Do you have any history of over due payments?</p>
-                <Select {...register("HistoryOfOverduePayments", { required: true })}>
-                <MenuItem value="1">Yes</MenuItem>
-                <MenuItem value="0">No</MenuItem>
-                </Select>
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>What is your maximum bill amount over the last 6 months?</p>
+                        <TextField variant="outlined" type="number" {...register("MaxBillAmountOverLast6Months", {required: true, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
 
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>What is your maximum bill amount over the last 6 months?</p>
-                <TextField variant="outlined" type="number" {...register("MaxBillAmountOverLast6Months", {required: true, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>What is your maximum payment amount over the last 6 months?</p>
+                        <TextField variant="outlined" type="number" {...register("MaxPaymentAmountOverLast6Months", {required: true, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>How many months did you do high spending over the last 6 months?</p>
+                        <TextField variant="outlined" type="number" {...register("MonthsWithHighSpendingOverLast6Months", {required: true, max: 6, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>How many months did you do low spending over the last 6 months?</p>
+                        <TextField variant="outlined" type="number" {...register("MonthsWithLowSpendingOverLast6Months", {required: true, max: 6, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>How many months did you have no money over the last 6 months?</p>
+                        <TextField variant="outlined" type="number" {...register("MonthsWithZeroBalanceOverLast6Months", {required: true, max: 6, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>What was your most recent bill amount?</p>
+                        <TextField variant="outlined" type="number" {...register("MostRecentBillAmount", {required: true, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>What was your most recent payment amount?</p>
+                        <TextField variant="outlined" type="number" {...register("MostRecentPaymentAmount", {required: true, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>How many months did you have over due?</p>
+                        <TextField variant="outlined" type="number" {...register("TotalMonthsOverdue", {required: true, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    
+                    <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
+                        <p>How many times did you have over due?</p>
+                        <TextField variant="outlined" type="number" {...register("TotalOverdueCounts", {required: true, min: 0})} />
+                        <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                                
+                    <br></br>
+                    <br></br>
 
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>What is your maximum payment amount over the last 6 months?</p>
-                <TextField variant="outlined" type="number" {...register("MaxPaymentAmountOverLast6Months", {required: true, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-            
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>How many months did you do high spending over the last 6 months?</p>
-                <TextField variant="outlined" type="number" {...register("MonthsWithHighSpendingOverLast6Months", {required: true, max: 6, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-            
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>How many months did you do low spending over the last 6 months?</p>
-                <TextField variant="outlined" type="number" {...register("MonthsWithLowSpendingOverLast6Months", {required: true, max: 6, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-            
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>How many months did you have no money over the last 6 months?</p>
-                <TextField variant="outlined" type="number" {...register("MonthsWithZeroBalanceOverLast6Months", {required: true, max: 6, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-            
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>What was your most recent bill amount?</p>
-                <TextField variant="outlined" type="number" {...register("MostRecentBillAmount", {required: true, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-            
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>What was your most recent payment amount?</p>
-                <TextField variant="outlined" type="number" {...register("MostRecentPaymentAmount", {required: true, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-            
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>How many months did you have over due?</p>
-                <TextField variant="outlined" type="number" {...register("TotalMonthsOverdue", {required: true, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-            
-            <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 200 }}>
-                <p>How many times did you have over due?</p>
-                <TextField variant="outlined" type="number" {...register("TotalOverdueCounts", {required: true, min: 0})} />
-                <FormHelperText>Required</FormHelperText>
-            </FormControl>
-                        
-            <br></br>
-            <br></br>
+                    <Button variant="contained" type="submit">Submit</Button>
+                </form>`
 
-            <Button variant="contained" type="submit">Submit</Button>
-          </form>
-
-          <br></br>
-          
-          <p>Credit: {(predicted) ? "Accepted" : "Denied"}</p>
-        </Container>
+                <Divider variant="middle" />
+                
+                <h3>Credit: {(predicted===-1) ? "" : (predicted) ? "Accepted" : "Denied"}</h3>
+                </Container>
+                <Container maxWidth="sm">
+                    <div id='button_result_table'></div>
+                </Container>
+        </div>
     );
 }
 
